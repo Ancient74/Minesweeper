fe580a9685d32db9dda378c631075d885b17abe7
Ancient <yura.ruban2011@gmail.com>
2018-12-25 21:24:09 +0200

    Initial commit

---
+++ Minesweeper/Cell.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Minesweeper
+{
+    public class Cell
+    {
+        private readonly int i;
+        private readonly int j;
+        private readonly float x;
+        private readonly float y;
+        private readonly float w;
+        public bool Shown { get; set; }
+        public bool Bomb { get; set; }
+        public bool Flag { get; set; }
+        public bool GameOver { get; set; } = false;
+        public int BombsNear { get; private set; }
+        public Cell(int i,int j,float w)
+        {
+            this.i = i;
+            this.j = j;
+            x = i * w;
+            y = j * w;
+            this.w = w;
+            Shown = false;
+            Bomb = false;
+        }
+        public void SetFlag() { if (!Shown) Flag = !Flag; else if(!GameOver) Flag = false; }
+        public bool Show(Cell[,]grid)
+        {
+            if (!Flag)
+            {
+                if (BombsNear == 0)
+                {
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        int xoff = i + this.i;
+                        if (xoff < 0 || xoff == grid.GetLength(0)) continue;
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int yoff = j + this.j;
+                            if (yoff < 0 || yoff == grid.GetLength(1)) continue;
+                            if (!grid[xoff, yoff].Shown)
+                            {
+                                Shown = true;
+                                grid[xoff, yoff].Show(grid);
+                            }
+                        }
+                    }
+                }
+                else if (BombsNear > 0)
+                {
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        int xoff = i + this.i;
+                        if (xoff < 0 || xoff == grid.GetLength(0)) continue;
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int yoff = j + this.j;
+                            if (yoff < 0 || yoff == grid.GetLength(1)) continue;
+                            if (!grid[xoff, yoff].Shown)
+                            {
+                                Shown = true;
+
+                            }
+                        }
+                    }
+                }
+                else //game over
+                {
+                    for (int i = 0; i < grid.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < grid.GetLength(1); j++)
+                        {
+                            grid[i, j].Shown = true;
+                            grid[i, j].GameOver = true;
+                        }
+
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Count(Cell[,]grid)
+        {
+            if (Bomb) { BombsNear = -1;return; }
+
+            for (int i = -1; i <= 1; i++)
+            {
+                int xoff = i + this.i;
+                if (xoff < 0 || xoff == grid.GetLength(0)) continue;
+                for (int j = -1; j <= 1; j++)
+                {
+                    int yoff = j + this.j;
+                    if (yoff < 0 || yoff == grid.GetLength(1)) continue;
+                    if (grid[xoff, yoff].Bomb)
+                    {
+                        BombsNear++;
+                    }
+                    
+                }
+            }
+        }
+
+        public void Draw(Graphics g)
+        {
+            float xCenter = x + w * 0.2f, yCenter = y + w * 0.2f;
+            if (!Shown && !Flag)
+            {
+                g.FillRectangle(Brushes.Gray, x, y, w - 1, w - 1);
+                
+            }
+            if(!Flag && Shown)
+            {
+               
+                if (!Bomb)
+                {
+                    g.FillRectangle(Brushes.White, x, y, w - 1, w - 1);
+                    if (BombsNear > 0)
+                        g.DrawString(BombsNear.ToString(), new Font(FontFamily.GenericSerif, 14), Brushes.Black, xCenter - 1, yCenter - 5);
+                }
+                else if (Bomb)
+                {
+                    g.FillRectangle(Brushes.White, x, y, w - 1, w - 1);
+                    g.FillEllipse(Brushes.Red, xCenter, yCenter, w * 0.5f, w * 0.5f);
+                }
+                
+            }else if (Flag)
+                {
+                g.FillRectangle(Brushes.Gray, x, y, w - 1, w - 1);
+                g.FillPolygon(Brushes.SkyBlue, new PointF[] { new PointF(x, y), new PointF(x, y+w*0.8f), new PointF(x+w*0.8f, y) });
+                }
+            if (GameOver)
+            {
+                if (Flag && Bomb)
+                {
+                    g.FillRectangle(Brushes.White, x, y, w - 1, w - 1);
+                    g.FillEllipse(Brushes.Green, xCenter, yCenter, w * 0.5f, w * 0.5f);
+                }
+                if (Flag && !Bomb)
+                {
+                    g.FillRectangle(Brushes.White, x, y, w - 1, w - 1);
+                    g.FillEllipse(Brushes.Black, xCenter, yCenter, w * 0.5f, w * 0.5f);
+                }
+            }
+        }
+    }
+}
---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        Cell[,] grid;
+        Graphics g;
+        Bitmap img;
+        int cols, rows;
+        int width, height;
+        int bombsCount = 20;
+        float scale;
+        bool gameOver = false;
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!IsGamePassed() && !gameOver)
+            {
+                int x = (int)Math.Floor(e.X / scale);
+                int y = (int)Math.Floor(e.Y / scale);
+                if (e.Button == MouseButtons.Left)
+                    gameOver = grid[x, y].Show(grid);
+                else if (e.Button == MouseButtons.Right)
+                {
+                    grid[x, y].SetFlag();
+                }
+
+                UpdateField();
+                if (gameOver)
+                {
+                    MessageBox.Show("Вы проиграли");
+                }
+                if (IsGamePassed())
+                {
+                    ShowAll();
+                    MessageBox.Show("Победа!");
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            bombsCount = (int)numericUpDown1.Value;
+
+            grid = new Cell[cols, rows];
+            List<int[]> choice = new List<int[]>();
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i, j] = new Cell(i, j, scale);
+
+                    choice.Add(new int[] { i, j });
+                }
+            }
+            for (int i = 0; i < bombsCount; i++)
+            {
+                int index = r.Next(0, choice.Count);
+                int x = choice[index][0];
+                int y = choice[index][1];
+                choice.RemoveAt(index);
+                grid[x, y].Bomb = true;
+            }
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i, j].Count(grid);
+
+
+                }
+            }
+            UpdateField();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            height = pictureBox1.Height;
+            width = pictureBox1.Width;
+            img = new Bitmap(width, height);
+            g = Graphics.FromImage(img);
+            scale = 24;
+            cols = (int)Math.Floor(width / scale);
+            rows = (int)Math.Floor(height / scale);
+            pictureBox1.Width = (int)(cols * scale);
+            pictureBox1.Height = (int)(rows * scale);
+            numericUpDown1.Value = bombsCount;
+            Button1_Click(null, null);
+
+
+        }
+        void UpdateField()
+        {
+            g.Clear(Color.Black);
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i, j].Draw(g);
+                }
+            }
+
+            pictureBox1.Image = img;
+        }
+        bool IsGamePassed()
+        {
+            
+            int markedBombs = 0;
+            int flags = 0;
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if (grid[i, j].Flag)
+                        flags++;
+                    if (grid[i, j].Bomb && grid[i, j].Flag)
+                        markedBombs++;
+
+                }
+            }
+            return markedBombs == flags && bombsCount == markedBombs;
+        }
+        void ShowAll()
+        {
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i, j].GameOver = true;
+                    grid[i, j].Shown = true;
+                }
+            }
+            UpdateField();
+        }
+    }
+}
